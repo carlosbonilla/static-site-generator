fb7a08ac14d69ffc2fcb49880437a4db99d34c29
Carlos Bonilla <carlosbonilla@isboli.com>
2024-03-13 20:24:51 -0600

    Add HTMLNode class to main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from textnode import TextNode
+from htmlnode import HTMLNode
 
 def main():
   text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
   print(text_node)
+
+  html_node = HTMLNode("a", "This is a text node", "bold", {"href": "https://www.boot.dev", "target": "_balnk"})
+  print(html_node)
 
 main()
