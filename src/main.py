a5c64fab242a98f5722ca8b7d98ea7b54e2cbf15
Carlos Bonilla <carlosbonilla@isboli.com>
2024-03-17 08:43:57 -0600

    Implement LeafNode class and add test case

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import TextNode
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 def main():
   text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
@@ -7,5 +8,8 @@ def main():
 
   html_node = HTMLNode("a", "This is a text node", "bold", {"href": "https://www.boot.dev", "target": "_balnk"})
   print(html_node)
+
+  leaf_node = LeafNode("a", "boot.dev", {"href": "https://www.boot.dev", "target": "_balnk"})
+  print(leaf_node)
 
 main()
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -14,3 +14,9 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_none(self):
         node = HTMLNode(None, None, None, None)
         self.assertEqual(node.props_to_html(), "")
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "boot.dev", None)
+        self.assertEqual(node.to_html(), "<p>boot.dev</p>")
+        
