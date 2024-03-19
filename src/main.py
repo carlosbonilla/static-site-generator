288b8db240f81c73446842892693010ecab5082e
Carlos Bonilla <carlosbonilla@isboli.com>
2024-03-19 16:43:28 -0600

    Fix typo in LeafNode class and add ParentNode class

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from textnode import TextNode
-from htmlnode import HTMLNode
-from htmlnode import LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 def main():
   text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
@@ -11,5 +10,8 @@ def main():
 
   leaf_node = LeafNode("a", "boot.dev", {"href": "https://www.boot.dev", "target": "_balnk"})
   print(leaf_node)
+
+  parent_node = ParentNode("div", [LeafNode("p", "boot.dev", {"href": "https://www.boot.dev", "target": "_balnk"})], {"class": "container"})
+  print(parent_node)
 
 main()
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -19,4 +19,44 @@ class TestLeafNode(unittest.TestCase):
     def test_to_html(self):
         node = LeafNode("p", "boot.dev", None)
         self.assertEqual(node.to_html(), "<p>boot.dev</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "boot.dev", None)
+        self.assertEqual(node.to_html(), "boot.dev")
+
+    def test_to_html_no_value(self):
+        node = LeafNode("p", None, None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+    
+    def test_to_html_with_props(self):
+        node = LeafNode("a", "boot.dev", {"href": "https://www.boot.dev"})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.boot.dev\">boot.dev</a>")
+
         
+class TestParentNode(unittest.TestCase):
+    def test_to_html(self):
+        node = ParentNode("div", [LeafNode("p", "boot.dev")], None)
+        self.assertEqual(node.to_html(), "<div><p>boot.dev</p></div>")
+        
+    def test_to_html_no_tag(self):
+        node = ParentNode(None, [LeafNode("p", "boot.dev")], None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_to_html_no_children(self):
+        node = ParentNode("div", None, None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_to_html_empty_children(self):
+        node = ParentNode("div", [], None)
+        self.assertEqual(node.to_html(), "<div></div>") 
+
+    def test_to_html_nested(self):
+        node = ParentNode("div", [ParentNode("p", [LeafNode("a", "boot.dev", {"href": "https://www.boot.dev"})])], None)
+        self.assertEqual(node.to_html(), "<div><p><a href=\"https://www.boot.dev\">boot.dev</a></p></div>")
+
+    def test_to_html_with_grandchildren(self):
+        node = ParentNode("div", [ParentNode("p", [LeafNode("a", "boot.dev", {"href": "https://www.boot.dev"})]), ParentNode("p", [LeafNode("a", "boot.dev", {"href": "https://www.boot.dev"})])], None)
+        self.assertEqual(node.to_html(), "<div><p><a href=\"https://www.boot.dev\">boot.dev</a></p><p><a href=\"https://www.boot.dev\">boot.dev</a></p></div>")
